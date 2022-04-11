ba6af1cbfaaa174fb2fc9f829dd5c4722e0ab353
damianzhenxiaozhi <8603583+damianzhenxiaozhi@users.noreply.github.com>
2022-04-12 07:40:18 +0800

    Add files via upload

---
+++ Common/src/main/java/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    private Animal animal;
+
+    public Person(Animal animal) {
+        this.animal = animal;
+    }
+
+    public void feed() {
+        System.out.println("I'm feeding...");
+        animal.eat();
+    }
+}
